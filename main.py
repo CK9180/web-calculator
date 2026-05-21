a2121d6e3a4d93204974d779c8a95ca5375b2ca4
peachteaswag11 <sshreyaa64@gmail.com>
2026-05-21 09:31:32 +0800

    update code

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
 import math
 
 I = float(input("Your number is: "))
 
 QN = input("Do you need the log operator? (Yes or No): ")
 
-# FIX 1: correct way to compare strings
+
 if QN == "Yes" or QN == "yes":
-    print("Your answer is:")
-    print(math.log10(I))
+    print("Your answer is: ", math.log10(I))
+     
 
 else:
     I2 = float(input("Your second number is: "))
-    I3 = input("Your operation is (+(plus), -(minus), *(times), /(divide), %(remainder)): ")
-
+    I3 = input("Your operation is: ")
+ 
     if I3 == "+":
-        print("Your answer is:")
-        print(I + I2)
-
+        print("Your answer is: ", I + I2)
+       
     elif I3 == "-":
-        print("Your answer is:")
-        print(I - I2)
+        print("Your answer is: ", I - I2)
+       
 
     elif I3 == "*":
         print("Your answer is:")
         print(I * I2)
 
     elif I3 == "/":
-        if I2 != 0:   # safety check
+        if I2 != 0:
             print("Your answer is:")
             print(I / I2)
         else:
-            print("Error: Cannot divide by zero")
+            print("Cannot divide by 0")
 
     elif I3 == "%":
         print("Your remainder is:")
         print(I % I2)
 
     else:
-        print("Invalid operation")
+        print("Invalid")
